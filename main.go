cc0e1eac8c0df242b17bd70fddaa0b99e4626200
Robert Oros <robertstefanoros@gmail.com>
2023-02-28 12:06:11 +0200

    Add structs

--- main.go
+++ main.go
@@ -18,14 +18,26 @@ type List struct {
 }
 
 type Board struct {
+	id int
 	Name string
 	lists []List
 }
 
+var database []Board
+
+
+func addBoard(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func delBoard(w http.ResponseWriter, r *http.Request) {
+
+}
+
 
 func main(){
-	fmt.Printf("Starting server at port 8000\n")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
