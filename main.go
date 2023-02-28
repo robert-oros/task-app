e58e6c194aed9ddff51cd44496f2d08a2059c391
loisnicoras <lois.nicoras@gottant.com>
2023-02-28 12:44:33 +0200

    Modify addBoard

--- main.go
+++ main.go
@@ -1,43 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/aws/aws-sdk-go/service/appregistry"
 )
 
 type Card struct {
-	id int
+	id   int
 	text string
 }
 
 type List struct {
-	id int
+	id    int
 	title string
 	cards []Card
 }
 
 type Board struct {
-	id int
-	Name string
+	id    int
+	Name  string
 	lists []List
 }
 
 var database []Board
 
-
 func addBoard(w http.ResponseWriter, r *http.Request) {
-
+	var b Board
+	err := json.NewDecoder(r.Body).Decode(&b)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "Board: %+v", b)
 }
 
 func delBoard(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
-func main(){
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+func main() {
+	fmt.Printf("Starting server at port 8081\n")
+	http.HandleFunc("/add_board", addBoard)
+	http.HandleFunc("remove_board/{boardId}", delBoard)
+	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
 	}
 }
