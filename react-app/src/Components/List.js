7dc647b9b778bedb5442caed2668d055006f5783
Robert Oros <robertstefanoros@gmail.com>
2023-03-02 13:52:15 +0200

    Modify

--- react-app/src/Components/List.js
+++ react-app/src/Components/List.js
@@ -1,8 +1,18 @@
-import Card from './Card'
+import Cards from './Cards'
 
 function List(props) {
   return (
     <div>
+      <ul>
+        <li>
+          <span className="label">{props.data.listId}</span>
+        </li>
+        <li>
+          <span className="label">{props.data.title}</span>
+        </li>
+      </ul>
+
+      <Cards cards={props.data.cards}/>
     </div>
   );
 }
