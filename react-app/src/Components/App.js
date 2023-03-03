7073bc5b027459e7e49fff87078a05d3ba3abce3
Robert Oros <robertstefanoros@gmail.com>
2023-03-03 11:51:54 +0200

    MOdify app.js

--- react-app/src/Components/App.js
+++ react-app/src/Components/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      boards: []
+      boards: [],
+      isLoaded: false
     }
   }
 
@@ -14,9 +15,10 @@ class App extends Component {
     fetch("http://localhost:8081/get_boards", {
       method: 'GET'
     })
-    .then(respose => respose.json())
+    .then(res => res.json())
     .then(data => {
-      this.setState({ boards : data})
+      this.setState({ boards: data})
+      this.setState({ isLoaded: true })
     })
   }
   
@@ -28,12 +30,18 @@ class App extends Component {
       })
     }
 
-    return (
-      <div>
-        <Navbar />
-        {boards}
-      </div>
-    );
+    if (this.state.isLoaded) {
+      return (
+        <div>
+          <Navbar />
+          {boards}
+        </div>
+      );
+    } else {
+      return (
+        <h4 style={{textAlign: "center", paddingTop: '50%', paddingBottom: '50%'}}>Loading Board...</h4>
+      );
+    }
   }
 }
 
